a5e978c9bee94e7f649e93e622697473cdf8bb6a
Alexis Gibson - Rogers <116273057+Monsterbirdlimited@users.noreply.github.com>
2022-11-16 19:22:01 -0600

    Update soscap.py

--- soscap.py
+++ soscap.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import sys
 import gi
+import os
+import datetime
+import time
 
 gi.require_version('Gst', '1.0')
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gst
-from gi.repository import GObject
-from gi.repository import Gtk
+from gi.repository import Gst, Gtk
+
+
 
 class GTK_Main:
     def __init__(self):
-        window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
+        window = Gtk.Window()
         window.set_title("SOSCap")
-        window.set_default_size(200, 50)
+        window.set_default_size(120, 50)
         window.connect("destroy", Gtk.main_quit, "WM destroy")
         vbox = Gtk.VBox()
         window.add(vbox)
@@ -23,17 +25,17 @@ class GTK_Main:
         vbox.pack_start(hbox, False, False, 0)
         hbox.set_border_width(10)
         hbox.pack_start(Gtk.Label(), False, False, 0)
-        self.button = Gtk.Button("Start")
+        self.button = Gtk.Button(label ="Start")
         self.button.connect("clicked", self.start_stop)
         hbox.pack_start(self.button, False, False, 0)
-        self.button2 = Gtk.Button("Quit")
+        self.button2 = Gtk.Button(label ="Quit")
         self.button2.connect("clicked", self.exit)
         hbox.pack_start(self.button2, False, False, 0)
         hbox.add(Gtk.Label())
         window.show_all()
 
         # Set up the gstreamer pipeline
-        self.player = Gst.parse_launch("pipewiresrc ! queue !  videoscale ! videoconvert ! vaapih264enc bitrate=6000 ! h264parse ! mux. pulsesrc device=alsa_output.pci-0000_00_1b.0.analog-stereo.monitor ! queue ! audioconvert ! audioresample ! faac bitrate=32000 ! matroskamux name=mux ! filesink location=/home/deck/Videos/SOSCap_.mkv sync=true")
+        self.player = Gst.parse_launch("pipewiresrc do-timestamp=True ! queue !  videoscale ! videoconvert ! x264enc key-int-max=12 cabac=1 bframes=2 ! h264parse ! mux. pulsesrc device=alsa_output.pci-0000_00_1b.0.analog-stereo.monitor ! queue ! audioconvert ! audioresample ! lamemp3enc bitrate=320 ! matroskamux name=mux ! filesink location=/home/deck/Videos/SOSCap.mkv sync=true")
         bus = self.player.get_bus()
         bus.add_signal_watch()
         bus.enable_sync_message_emission()
@@ -47,6 +49,9 @@ class GTK_Main:
         else:
             self.player.set_state(Gst.State.NULL)
             self.button.set_label("Start")
+            #import rename
+            #execfile('rename.py')
+            self.rename()
 
     def exit(self, widget, data=None):
         Gtk.main_quit()
@@ -56,6 +61,9 @@ class GTK_Main:
         if t == Gst.MessageType.EOS:
             self.player.set_state(Gst.State.NULL)
             self.button.set_label("Start")
+            #import rename
+            #execfile('rename.py')
+            self.rename()
         elif t == Gst.MessageType.ERROR:
             err, debug = message.parse_error()
             print("Error: %s" % err, debug)
@@ -67,16 +75,19 @@ class GTK_Main:
         if not struct:
             return
         message_name = struct.get_name()
-        if message_name == "prepare-xwindow-id":
-            # Assign the viewport
-            imagesink = message.src
-            imagesink.set_property("force-aspect-ratio", True)
-            imagesink.set_xwindow_id(self.movie_window.window.xid)
+
+    def rename(self, w):
+        f_path = "/home/deck/Videos/SOSCap.mkv"
+        t = os.path.getctime(f_path)
+        t_str = time.ctime(t)
+        t_obj = time.strptime(t_str)
+        form_t = time.strftime("SOSCap_%Y-%m-%d %H:%M:%S", t_obj)
+        form_t = form_t.replace(":", "꞉")
+        os.rename(
+            f_path, os.path.split(f_path)[0] + '/' + form_t + os.path.splitext(f_path)[1])
+
 
 
 Gst.init(None)
 GTK_Main()
-GObject.threads_init()
 Gtk.main()
-
-
